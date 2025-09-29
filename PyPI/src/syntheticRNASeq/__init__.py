f9c67fc51bba5eb9fef28fe5eefd54b5ccb3bb98
tkaralis <93835798+ttkaralis@users.noreply.github.com>
2025-09-29 20:00:09 +0100

    Add files via upload

---
+++ PyPI/src/syntheticRNASeq/__init__.py
@@ -0,0 +1,5 @@
+from .syntheticRNASeq import SingleSampleGenerator, MultipleSampleGenerator, ReplicateGenerator
+
+__all__ = ["SingleSampleGenerator"]
+__all__ = ["MultipleSampleGenerator"]
+__all__ = ["ReplicateGenerator"]
